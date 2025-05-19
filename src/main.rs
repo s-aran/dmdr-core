79528f08ccbf24e4f3acdf79ff56faeb3d2fdabf
s.aran <57449457+s-aran@users.noreply.github.com>
2025-05-20 00:48:12 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod model;
 use std::io::Read;
 use std::{fs::File, path::Path};
 
+use model::FieldInstance;
+use rustpython_parser::ast::StmtClassDef;
 use rustpython_parser::{Mode, parse};
 
 fn main() {
@@ -32,6 +34,45 @@ fn main() {
     let result = parse(buf.as_str(), Mode::Module, "<embedded>");
 
     for e in result.iter() {
-        println!("{:?}", e);
+        // println!("{:?}", e);
+
+        if !e.is_module() {
+            break;
+        }
+
+        let module = e.as_module().unwrap();
+
+        println!(
+            "--------------------------------------------------------------------------------"
+        );
+
+        // println!("{:?}", module.body);
+
+        let module_body = &module.body;
+
+        for e in module_body.iter() {
+            // println!("{:?}", e);
+
+            if !e.is_class_def_stmt() {
+                continue;
+            }
+
+            println!("============================================================");
+
+            let class = e.as_class_def_stmt().unwrap();
+            println!("{:?}", class);
+
+            for c in class.body.iter() {
+                println!("{:?}", c);
+            }
+
+            let a: FieldInstance = From::from(class);
+
+            println!("============================================================");
+        }
+
+        println!(
+            "--------------------------------------------------------------------------------"
+        );
     }
 }
--- src/model.rs
+++ src/model.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use rustpython_parser::ast::StmtClassDef;
+
 pub struct Model<F>
 where
     F: Filed,
@@ -8,6 +10,7 @@ where
     meta: Meta,
 }
 
+#[derive(Debug)]
 pub struct Attribute {
     key: String,
     value: String,
@@ -20,6 +23,7 @@ pub trait Filed {
     fn get_attributes(&self) -> &Vec<Attribute>;
 }
 
+#[derive(Debug)]
 pub struct FieldInstance {
     name: String,
     class_name: String,
@@ -42,6 +46,30 @@ impl Filed for FieldInstance {
 
     fn get_attributes(&self) -> &Vec<Attribute> {
         &self.attributes
+    }
+}
+
+impl From<&StmtClassDef> for FieldInstance {
+    fn from(value: &StmtClassDef) -> Self {
+        let name = value.name.as_str().to_string();
+        let class_name = value.name.as_str().to_string();
+
+        let base_class_expr = value.bases.get(0).unwrap(); // first only
+        let base_class = base_class_expr.as_attribute_expr().unwrap();
+        let base_class_name_expr = base_class.value.as_name_expr().unwrap();
+        let base_class_module_name = base_class_name_expr.id.as_str().to_string();
+        let base_class_name = base_class.attr.as_str().to_string();
+
+        println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        println!("{:?}", value.body);
+        println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        Self {
+            name,
+            class_name,
+            base_class_name: format!("{}.{}", base_class_module_name, base_class_name),
+            attributes: Vec::new(),
+        }
     }
 }
 
